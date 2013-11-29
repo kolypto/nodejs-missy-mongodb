2099850c1ebb33311ed41ba3f18fbc9d6a9d7f0f
Vartanyan Mark <kolypto@gmail.com>
2013-11-29 02:15:40 +0200

    Initial experimental release

--- README.md
+++ README.md
@@ -1,24 +1,52 @@
 Missy MongoDB driver
 ====================
 
-MongoDB database driver for Missy
+MongoDB database driver for [Missy](https://github.com/kolypto/nodejs-missy).
 
 Usage
 =====
 
+Creating a Schema
+-----------------
+
+Simple form:
+
 ```js
-var missy = require('missy'),
-    require('missy-mongodb')
+var missy = require('missy').loadDriver('mongodb')
+    ;
+
+var schema = new missy.Schema('mongodb://localhost/test', {
+    connect: {
+        // Native MongoClient options
+        db: {
+            journal: true,
+            native_parser: true
+        },
+        server: {
+            ssl: false,
+            logger: true
+        }
+    }
+});
+```
+
+Full form with manual driver initialization:
+
+```js
+var missy = require('missy').loadDriver('mongodb'),
+    mongodb = require('mongodb')
     ;
 
 // Driver
-var mongoDriver = new MongodbDriver(function(){
-    // Connecter function
-}, {
-    // driver options
-    journal: true
+var driver = new MongodbDriver(function(){ // Custom connecter function
+    // A promise for a client
+    return Q.nmcall(mongodb.MongoClient, 'connect',
+        'mongodb://localhost/test',
+        { db: { journal: true } }
+    ); // -> db
 });
 
 // Schema
-var schema = new missy.Schema(mongoDriver);
+var schema = new missy.Schema(driver);
+var schema = new missy.Schema('mongodb://localhost/test'); // short form
 ```
---
+++ tests/MongodbDriver-test.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var Q = require('q'),
+    _ = require('lodash'),
+    missy = require('missy')
+    ;
+require('../');
+
+/** Set up the Schema
+ */
+exports.setUp = function(callback){
+    this.schema = new missy.Schema(
+        process.env['MISSY_MONGODB'],
+        { connect: {
+            db: {
+                journal: false,
+                native_parser: false
+            },
+            server: {
+                ssl: false,
+                logger: false,
+                auto_reconnect: false
+            },
+            replSet: null,
+            mongos: null
+        } });
+
+    this.schema.connect()
+        .nodeify(callback);
+};
+
+exports.testMongodbDriver = function(test){
+    console.log('TEST');
+    test.done();
+};
+
+exports.tearDown = function(callback){
+    if (!this.schema)
+        return callback();
+
+    var schema = this.schema,
+        db = schema.getClient();
+
+    // Collect models' collections
+    var collections = _.map(schema.models, function(model){
+        return model.options.table;
+    });
+
+    Q()
+        // Load existing collection names
+        .then(function(){
+            return Q.nmcall(db, 'collectionNames');
+        })
+        // Remove collections
+        .then(function(existingCollections){
+            console.log('Existing collections: ', existingCollections);
+            console.log('Drop: ', collections);
+            return _.map(
+                    _.intersection(collections, existingCollections),
+                    function(collection){
+                        return function(){
+                            return Q.nmcall(db.collection(collection, 'drop'));
+                        };
+                    }
+            ).reduce(Q.when, Q(1));
+        })
+        .then(function(){
+            return schema.disconnect();
+        })
+        // Finish
+        .nodeify(callback);
+};
